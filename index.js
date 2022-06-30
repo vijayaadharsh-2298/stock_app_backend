6b3168cc33a1110457dbdf887dfb27f1a89ac399
Vijay M <vijay.munuswamy@learnship.com>
2022-07-01 00:55:06 +0530

    New commit added

--- index.js
+++ index.js
@@ -9,22 +9,50 @@ const url = "mongodb://localhost:27017";
 expresServer.use(express.json());
 expresServer.use(cors());
 
-
-expresServer.get('/', async function (req, res) {
-  const { companyId } = req.query;
-
-  const req_url =
-    'https://www.alphavantage.co/query?function=OVERVIEW&symbol=' + companyId + '&apikey=SQVTXIQMTBY2CVA2';
-
+expresServer.get("/getAllCompanyDetails", async (req, res) => {
   try {
-    let response = await axios({
-      method: 'GET',
-      url: req_url
-    });
-    res.send(response.data);
-  } catch (err) {
-    console.log(err)
+    await MongoClient.connect(url, async (err, db) => {
+      if (err) throw err;
+      const dbo = db.db('Stock_Details');
+      const response = await dbo.collection("CompanyStockDetails").find().toArray();
 
+      if (!!response && response.length > 0) {
+        res.status(200).send(response);
+      } else {
+        res.status(200).send({
+          code: 404,
+          message: "No details found"
+        })
+      }
+    })
+  } catch (error) {
+    res.status(400).send({
+      message: "Something went wrong",
+      error
+    })
+  }
+})
+
+expresServer.get('/getCompanyDetail', async (req, res) => {
+  try {
+    await MongoClient.connect(url, async (err, db) => {
+      if (err) throw err;
+      const dbo = db.db('Stock_Details');
+      const { companyId } = req.query;
+      const isAvailable = await dbo.collection("CompanyStockDetails").findOne({ Symbol: companyId });
+      if (!!isAvailable) {
+        res.status(200).send(isAvailable);
+      } else {
+        res.status(404).send({
+          message: "No details found"
+        })
+      }
+    });
+  } catch (error) {
+    res.status(400).send({
+      message: "Something went wrong",
+      error
+    })
   }
 });
 
@@ -42,20 +70,31 @@ expresServer.post('/', async (req, res) => {
         method: 'GET',
         url: req_url
       });
-      console.log(response.data);
 
       const isAvailable = await dbo.collection("CompanyStockDetails").findOne({ Symbol: companyId });
-      if (Object.keys(isAvailable).length === 0) {
-        dbo.collection('CompanyStockDetails').insertOne(response.data, (result) => {
-          res.send(response.data);
-          db.close();
-        })
+
+      if (response && response.data && Object.keys(response.data).length > 0) {
+        if (isAvailable === null) {
+          dbo.collection('CompanyStockDetails').insertOne(response.data, (result) => {
+            res.status(201).send({
+              message: "Successfully created"
+            });
+            db.close();
+          })
+        } else {
+          res.status(200).send({
+            message: "Already existed"
+          });
+        }
       } else {
-        res.send(response.data);
+        throw new Error("No details found.")
       }
 
     } catch (err) {
-      console.log(err);
+      res.status(400).send({
+        message: "Something went wrong",
+        error
+      });
     }
   });
 })
